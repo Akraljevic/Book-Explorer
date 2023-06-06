9a158d23dc86e86582cbdfb734442bc4955f2e95
Ante Kraljević <86553387+Akraljevic@users.noreply.github.com>
2023-06-06 16:04:51 +0200

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="footer" class="text-center">
+    <div class="red--text text--darken-3">Book-explorer 2021</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+#footer {
+  width: 100vw;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+  font-family: "Courier New", Courier, monospace;
+  font-size: large;
+}
+</style>
